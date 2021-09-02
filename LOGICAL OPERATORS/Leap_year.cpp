836a3abf12bed1623c0d88a22b533077f72b5eb4
Nathan_Zaldivar <nathan.a.zaldivar@gmail.com>
2021-09-02 14:17:30 -0400

    Leap year project
    Leap year project

---
+++ LOGICAL OPERATORS/Leap_year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+  int year;
+  std::cout << "Enter current year: ";
+  std::cin >> year;
+
+  if (year < 9999 && year > 1000) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+      std::cout << "Leap year!\n";
+    } else {
+      std::cout << "Not a leap year!\n";
+    }
+  }
+}

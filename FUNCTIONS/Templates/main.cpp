a35b7f6fc8e863ae2902e349ad5d4dd16b58b627
Nathan_Zaldivar <nathan.a.zaldivar@gmail.com>
2021-09-11 11:30:40 -0400

    Update main.cpp

--- FUNCTIONS/Templates/main.cpp
+++ FUNCTIONS/Templates/main.cpp
@@ -1,8 +1,10 @@
-// Replace these declarations with a template
+#include <iostream>
 
-template <typename T>
-T get_smallest(T num1, T num2) {
+#include "numbers.hpp"
+
+int main() {
   
-  return num2 < num1 ? num2 : num1;
- 
+  std::cout << get_smallest(100, 60) << "\n";
+  std::cout << get_smallest(2543.2, 3254.3) << "\n";
+  
 }

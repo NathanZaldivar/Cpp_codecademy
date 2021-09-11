c0059fe58970ebb50f3bedc8865f07c927bd19d6
Nathan_Zaldivar <nathan.a.zaldivar@gmail.com>
2021-09-11 11:11:01 -0400

    Create Conditional_evaluation.cpp

---
+++ Extra Logic/Conditional_evaluation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+int get_smallest(int num1, int num2) {
+  
+  return num2 < num1 ? num2 : num1; 
+  
+  // Value1 ? Value1 : Value 2
+  
+  // Logical explanation:
+  // If Value1 is true then the conditional assums the value of Value1, else Value2
+  
+  // Basically a If else statement
+  
+}
+
+double get_smallest(double num1, double num2) {
+  
+  return num2 < num1 ? num2 : num1;
+  
+}
+

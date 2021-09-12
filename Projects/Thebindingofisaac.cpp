a1448d2126ecd812de8703715a4df6adb49d8453
root <root@guest.guest>
2021-09-12 00:35:22 +0000

    hello

--- Projects/Thebindingofisaac.cpp
+++ Projects/Thebindingofisaac.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
 
+
 int main() {
 
-  int chaos = 0, choice = 0, health = 1, damage = 1;
-  bool backup = true, angel = true;
+  int chaos = 0, choice = 0, health = 1, damage = 1, test = 0;
+  bool backup = true, angel = true, boss_skip = false, bible = false, hush = false;
 
   std::string logo =  R"(
 ................................................................................
@@ -100,6 +101,190 @@ Isaac, watching through a crack in the door, trembled in fear. Scrambling around
       default:
         std::cout << "\nInvalid choice! Restarting.\n";
         break;
+ 
     }
   }
+ 
+
+
+
+ std::cout << "You enter the boss room and have a 50% chance to defeat the boss without taking damage.\n";
+
+ srand (time(NULL));
+
+ std::cout << "\nHIT ENTER TO FLIP A COIN\n";
+ system("python3 pause.py");
+
+ int chance = rand() % 2;
+ 
+ switch (chance) {
+
+  case 0:
+   std::cout << "Heads! you defeat the boss without losing any health\n";
+   break; 
+  
+  case 1:
+   std::cout << "Tails! you defeat the boss but take 1 hit of damage\n";
+   health--;
+   if (health < 1) {
+    std::cout << "you have no more health! you fall over and die :(\n";
+    return 0;
+   }
+   break;
+
+  default:
+   std::cout << "ERROR IN STAGE 1 BOSS COIN FLIP\n";
+   return 0;
+   break;
+
+ }
+ 
+ srand (time(NULL));
+ chance = rand() % 2;
+ backup = true;
+
+ while (backup == true) {
+  std::cout << "\n(Caves I-II)\n";
+  std::cout << "You find your self in a room filled with clots you have a couple options to clear the room\n";
+  std::cout << "  1) use a bomb to blow a door and escape\n";
+  std::cout << "  2) use REAL skill to kill all the clots, you have a 50% chance to clear the room without taking damage\n";
+  std::cout << "  3) use your ace of hearts to turn all the clots to possible health\n";
+  std::cout << "  4) blow up a random rock...\n";
+  std::cout << "$: ";
+
+  std::cin >> choice;
+
+  switch (choice) {
+
+   case 1:
+    std::cout << "You run away with your tail between your legs! While running a clot hits you with a lucky shot! You lose 1 health point!\n";
+    health--;
+    backup = false;
+    break;
+   
+   case 2:
+    if (chance == 0) {
+     std::cout << "You use your inner lost to dodge all clot attacks and clear the room giving you a key, you use the key to open a chest health inside!\n";
+     health++;
+    } else {
+     std::cout << "You try to dodge the clots attacks but clots are Bastards and should have not been added into the game! You lose 1 health point.\n";
+     health--;
+    }
+    if (health < 1) {
+     std::cout << "You ran out of health! GAME OVER...\n";
+     return 0;
+    }
+    backup = false;
+    break;
+
+   case 3:
+    std::cout << "You convert all clots into random hearts but its all rotten hearts! your health stays the same!\n";
+    backup = false;
+    break;
+
+   case 4:
+    std::cout << "You blow up a random rock and only by dumb luck does a trap door appear allowing you to skip the next boss...\n";
+    backup = false;
+    boss_skip = true;
+    chaos += 25;
+    break;
+
+   default:
+    std::cout << "Invalid choice, restarting...\n";
+    break;
+  }
+ } 
+
+
+ srand (time(NULL));
+ chance = rand() % 2;
+ backup = true;
+
+
+ if (boss_skip != true) {
+
+  std::cout << "You enter the boss room and have a 50% chance to defeat the boss without taking damage.\n";
+  std::cout << "HIT ENTER TO FLIP A COIN\n";
+  
+  system("python3 pause.py");
+
+  switch (chance) {
+
+   case 0:
+    std::cout << "Heads! you defeat the boss without losing any health\n";
+    break;
+
+   case 1:
+    std::cout << "Tails! you defeat the boss but take 1 hit of damage.\n";
+    health--;
+    if (health < 1) {
+     std::cout << "You have no more health! you fall over and die :(\n";
+     return 0;
+    }
+    break;
+
+   default:
+    std::cout << "ERROR IN STAGE 2 BOSS COIN FLIP\n";
+    return 0;
+    break;
+  }
+ } else {
+  std::cout << "You skip the boss room by going through the hatch!\n";
+  boss_skip = false;
+ }
+
+ srand (time(NULL));
+ chance = rand() % 2;
+
+ while (backup == true) {
+  std::cout << "\n(Depths I-II)\n";
+  std::cout << "You find your self in item room with 4 chests, choose which chest to open\n";
+  std::cout << "  1) Open the red chest\n";
+  std::cout << "  2) Open the normal chest\n";
+  std::cout << "  3) Open the golden chest\n";
+  std::cout << "  4) Open the stone chest\n";
+  std::cout << "$: ";
+
+  std::cin >> choice;
+
+  switch (choice) {
+   
+   case 1:
+    if (angel == true) {
+     std::cout << "The red chest teleports you to a angle room with a Bible! You pick up the bible and head to the boss room...\n";
+     bible = true;
+     backup = false;
+     break;
+    } else {
+     std::cout << "The red chest teleports you to a devil room with health pickup! Gain 1 hit point.\n";
+     health++;
+    }
+    chaos += 25;
+    backup = false;
+    break;
+
+   case 2:
+    std::cout << "The normal chest turns out to be a haunted chest! lose 1 hit point";
+    health--;
+    backup = false;
+    break;
+
+   case 3:
+    std::cout << "You open the golden chest to find a item called ???, you wonder what use it could have...\n";
+    hush = true;
+    backup = false;
+    chaos += 25;
+    break;
+
+   case 4:
+    std::cout << "The stone chest had a soul heart! gain 1 hit point\n";
+    health++;
+    backup = false;
+    break;
+
+   default:
+    std::cout << "Invalid choice! Restarting...\n";
+    break; 
+  }
+ }
 }

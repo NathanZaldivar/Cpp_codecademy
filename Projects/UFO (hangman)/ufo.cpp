d78f2da0048d6dca560f1a10f889ed62783dca81
Nathan Zaldivar <nathan.a.zaldivar@gmail.com>
2021-09-16 14:39:50 -0400

    Final push

--- Projects/UFO (hangman)/ufo.cpp
+++ Projects/UFO (hangman)/ufo.cpp
@@ -5,43 +5,70 @@
 
 int main() {
 
-  std::string codeword = "codecademy";
-  std::string answer = "__________";
+  std::string codeword = lower(codeword_chooser());
+  hide();
+  std::string answer = codeword_ghost(codeword);
 
   int misses = 0;
+  int miss_limit = 7;
+
+  bool debug = false;
+  bool codeword_choice;
 
   greet();
 
-  while (answer != codeword && misses < 7) {
+  while (answer != codeword && misses < miss_limit && debug == false) {
 
-    char choice = input_char("Letter: ");
+    display_misses(misses);
+
+    long_print(answer);
+
+    char choice = input("Letter: ", 'c');
 
     int ind = index(codeword, choice);
 
-    if (ind == -1) {
+    int ind_answer = index(answer, choice);
 
-      print("WRONG!");
+    // ind is used to determine if the user got the answer correct, -1 indicates that the char can not be found in the selected string, See ufo_functions.cpp.
+
+    if (ind == -1 && ind_answer == -1) {
+
+      print("incorrect! " + str((miss_limit - 1) - misses) + " Tries remaining.");
 
       misses++;
 
+    } else if (ind != -1 && ind_answer == -1) {
+
+      answer = answer_builder(answer, codeword, choice);
+
+      print("Correct!");
+
     } else {
 
-      answer = replace_char(answer, choice, ind);
-
-      print(answer);
-
-      print("CORRECT!");
+      print("Character already chosen! Restarting.");
 
     }
-
   }
+// Debug sandbox
 
+// Debug sandbox
   if (answer == codeword) {
 
-    print("Hooray! You saved the person and earned a medal of honor!");
+    print("Congragulations you won!");
 
   } else {
 
-    print("Oh no! The UFO just flew away with another person!");
+    print("The UFO flies away! GAME OVER");
+
+  }
+
+  print("Codeword was " + codeword);
+
+  char play_again = lower(input("play again?[y or n] ", 'c'));
+
+  if (play_again == 'y') {
+
+    main();
+
   }
 }
--- Projects/UFO (hangman)/ufo_functions.hpp
+++ Projects/UFO (hangman)/ufo_functions.hpp
@@ -3,7 +3,7 @@
 
 
 
-// Simple print functions
+// Simple print function
 template <typename T>
 void print(T string, bool new_line = true) {
 
@@ -18,10 +18,78 @@ void print(T string, bool new_line = true) {
   }
 }
 
+template <typename T>
+void long_print(T string) {
+
+  std::cout << "\n\n" << string << "\n\n";
+
+}
+
+template <typename T>
+int index(T data, char character, int skip_int = 0) {
+
+  int interval = 0;
+
+  for (int i = 0; i < data.size(); i++) {
+
+    if (data[i] == character) {
+
+      if (interval == skip_int) {
+
+        return i;
+
+      } else {
+
+        interval++;
+
+      }
+    }
+  }
+
+  return -1;
+
+}
+
+template <typename T>
+int index(T data, int character, int skip_int = 0) {
+
+  int interval = 0;
+
+  for (int i = 0; i < data.size(); i++) {
+
+    if (data[i] == character) {
+
+      if (interval == skip_int) {
+
+        return i;
+
+      } else {
+
+        interval++;
+
+      }
+    }
+  }
+
+  return -1;
+
+}
+
+char input(std::string input_string, char type);
+char lower(char character);
 
 void greet();
 void display_misses(int misses);
-int index(std::string data, char character);
-std::string input_str(std::string input_string);
-char input_char(std::string input_string);
-std::string replace_char(std::string selected_string, char character, std::vector position);
+void hide();
+
+int intcur(std::string string_data, char character);
+int input(std::string input_string, int type);
+
+std::string codeword_ghost(std::string codeword);
+std::string codeword_chooser();
+std::string input(std::string input_string, std::string type);
+std::string str(int integer);
+std::string replace_char(std::string selected_string, char character, int position);
+std::string replace_all(std::string selected_string, char character, char selected_character);
+std::string answer_builder(std::string answer, std::string codeword, char choice);
+std::string lower(std::string string);

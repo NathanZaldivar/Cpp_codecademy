33fcb50c264e088e02a3bae29f88144630e3313e
Nathan_Zaldivar <nathan.a.zaldivar@gmail.com>
2021-10-02 13:31:12 -0400

    Update header.hpp

--- Projects/Python++/header.hpp
+++ Projects/Python++/header.hpp
@@ -7,13 +7,24 @@ char input(std::string input_string, char type);
 char lower(char character);
 
 void hide();
+//// print family
 void print(std::vector<int> data, bool new_line = false, bool no_seperator = false);
 void print(std::vector<std::string> data, bool new_line = false, bool no_seperator = false);
 void print(std::vector<char> data, bool new_line = false, bool no_seperator = false);
 void print(int data, bool new_line = true);
 void print(std::string data, bool new_line = true);
 void print(char data, bool new_line = true);
+//// print family
 
+//// index family
+int index(std::string data, char character, int skip_int = 0);
+int index(std::string data, int character_int, int skip_int = 0);
+int index(std::string data, std::string character, int skip_int = 0);
+int index(std::vector<std::string> data, char character, int skip_int = 0);
+int index(std::vector<std::string> data, std::string character, int skip_int = 0);
+int index(std::vector<char> data, char character, int skip_int = 0);
+int index(std::vector<int> data, int character, int skip_int = 0);
+//// index family
 int intcur(std::string string_data, char character);
 int input(std::string input_string, int type);
 int coin_flip();
